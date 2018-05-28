51ce2ee7ddb699fe971ae0f3759e5ef838266e66
lleeych <34095050+lleeych@users.noreply.github.com>
2018-05-28 21:40:27 +0800

    ch1.2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { OcrHomeComponent } from './ocr-home/ocr-home.component';
+import {OcrLoginComponent} from './ocr-login/ocr-login.component';
+import {OcrPersonalComponent} from './ocr-personal/ocr-personal.component';
+import {OcrRegisterComponent} from './ocr-register/ocr-register.component';
+import {OcrHistoryComponent} from './ocr-history/ocr-history.component';
 
 const ROUTES: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: OcrHomeComponent},
+  { path: 'login', component: OcrLoginComponent },
+  { path: 'register', component: OcrRegisterComponent },
+  { path: 'history', component: OcrHistoryComponent },
+  { path: 'personal', component: OcrPersonalComponent }
 ];
 
 @NgModule({

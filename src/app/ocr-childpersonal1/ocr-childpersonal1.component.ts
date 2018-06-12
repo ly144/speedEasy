f1d242386d746e64251ea79a29bb3276cdab2adc
lleeych <34095050+lleeych@users.noreply.github.com>
2018-06-12 19:01:18 +0800

    ch1.4

--- src/app/ocr-childpersonal1/ocr-childpersonal1.component.ts
+++ src/app/ocr-childpersonal1/ocr-childpersonal1.component.ts
@@ -1,20 +1,15 @@
-import { Component, OnInit , ViewEncapsulation} from '@angular/core';
-import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-ocr-childpersonal1',
   templateUrl: './ocr-childpersonal1.component.html',
   styleUrls: ['./ocr-childpersonal1.component.css'],
-  encapsulation: ViewEncapsulation.None
 })
 export class OcrChildpersonal1Component implements OnInit {
 
-  constructor(private modalService: NgbModal) { }
+  constructor() { }
 
   ngOnInit() {
   }
 
-  openchildpersonal1(content) {
-    this.modalService.open(content, {backdropClass: 'light-blue-backdrop', centered: true, size: 'sm'});
-  }
 }
